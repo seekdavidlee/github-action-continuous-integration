4cfed3909ff505e43054dc8bace1d622ef4d4fd2
ENG KEONG DAVID LEE <36802330+seekdavidlee@users.noreply.github.com>
2022-05-13 16:24:54 -0500

    changes (#2)

--- MyWebApi/WeatherForecastService.cs
+++ MyWebApi/WeatherForecastService.cs
@@ -2,7 +2,7 @@
 {
 	public class WeatherForecastService
 	{
-		private static List<WeatherForecast> _weatherForecasts = new List<WeatherForecast>();
+		private List<WeatherForecast> _weatherForecasts = new List<WeatherForecast>();
 
 		public void Add(WeatherForecast weatherForecast)
 		{
@@ -27,11 +27,6 @@
 		public WeatherForecast? Get(DateTime dateTime)
 		{
 			return _weatherForecasts.SingleOrDefault(x => x.Date == dateTime);
-		}
-
-		public void Clear()
-		{
-			_weatherForecasts.Clear();
 		}
 	}
 }
